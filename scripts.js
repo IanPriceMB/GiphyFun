7af8a2cd9ef71df1f13b8ee0e8fce18240802d17
IPMaddBuddha <37713898+IanPriceMB@users.noreply.github.com>
2018-06-24 15:53:37 -0600

    finally finished adding the still and animated states

--- scripts.js
+++ scripts.js
@@ -9,18 +9,40 @@ $.ajax({
     method: "GET"
     }).then(function(response) {
     console.log(response) 
+
     for (var i = 0; i < response.data.length; i++){
+        var animated = response.data[i].images.fixed_height.url;
+        var still = response.data[i].images.fixed_height_still.url;
+
         var gameDiv = $("<div id='gameLanding'" + i + ">")
         var rating = response.data[i].rating;
         var pOne = $("<p>").text("Rating: " + rating);
         gameDiv.append(pOne);
         var imgURL = response.data[i].images.original.url;
-        var image = $("<img>").attr("src", imgURL);
+        var image = $("<img>").attr("src", still);
+        image.attr("data-still", still);
+        image.attr("data-animate", animated);
+        image.addClass('game-image')
+        image.attr("data-state", "still");
         gameDiv.append(image);
         $(".imageLanding").prepend(gameDiv);
     }
     });
     }
+$(document).on("click", ".game-image", function() {
+
+  var state = $(this).attr("data-state");
+
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  }
+  else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+});
+  
 
 function renderButtons() {
     $(".buttonLanding").empty();
