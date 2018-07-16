e4ee9df25b386f6864ae9226dbfb91536368c739
Ian Price <ianpriceve@gmail.com>
2018-07-15 22:41:19 -0600

    strapped it in some boots and cleaned up the code

--- scripts.js
+++ scripts.js
@@ -1,34 +1,42 @@
 $(document).ready(function() {
-var games = ["Final Fantasy VII", "The Witcher", "League of Legends", "Banjo-Kazooie"];
+const games = ["Final Fantasy VII", "The Witcher", "League of Legends", "Banjo-Kazooie"];
 
 function displayGameInfo() {
-var game = $(this).attr("data-name");
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + game + "&api_key=kWSeR23xjNTajEuvZtpJ7hnSTQeQTyk6&limit=10";
+let game = $(this).attr("data-name");
+let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + game + "&api_key=kWSeR23xjNTajEuvZtpJ7hnSTQeQTyk6&limit=10";
+
 $.ajax({
     url: queryURL,
     method: "GET"
     }).then(function(response) {
-    console.log(response) 
 
     for (var i = 0; i < response.data.length; i++){
-        var animated = response.data[i].images.fixed_height.url;
-        var still = response.data[i].images.fixed_height_still.url;
+        const animated = response.data[i].images.fixed_height.url;
+        const still = response.data[i].images.fixed_height_still.url;
 
-        var gameDiv = $("<div id='gameLanding'" + i + ">")
-        var rating = response.data[i].rating;
-        var pOne = $("<p>").text("Rating: " + rating);
-        gameDiv.append(pOne);
-        var imgURL = response.data[i].images.original.url;
-        var image = $("<img>").attr("src", still);
+        const gameDiv = $("<div id='gameLanding'" + i + ">")
+        const rating = response.data[i].rating;
+        const p = $("<p>").text("Rating: " + rating);
+
+        gameDiv.append(p);
+        const image = $("<img>").attr("src", still);
+        
         image.attr("data-still", still);
         image.attr("data-animate", animated);
         image.addClass('game-image')
         image.attr("data-state", "still");
         gameDiv.append(image);
-        $(".imageLanding").prepend(gameDiv);
+
+        if (i%2===0){
+          $("#imageLanding1").prepend(gameDiv);
+        } else {
+          $("#imageLanding2").prepend(gameDiv);
+        }
+
     }
     });
     }
+
 $(document).on("click", ".game-image", function() {
 
   var state = $(this).attr("data-state");
@@ -43,23 +51,28 @@ $(document).on("click", ".game-image", function() {
   }
 });
   
-
 function renderButtons() {
     $(".buttonLanding").empty();
     for (var i = 0; i < games.length; i++) {
       var a = $("<button>");
-      a.addClass("game-btn");
+      a.addClass("game-btn btn btn-secondary");
       a.attr("data-name", games[i]);
       a.text(games[i]);
       $(".buttonLanding").append(a);
     }
   }
+  renderButtons();
+  
 $("#addGame").on("click", function(event) {
     event.preventDefault();
     var game = $("#gameInput").val().trim();
-    games.push(game);
-    renderButtons();
+    if(!game) {
+      alert('specify a game')
+    } else {
+      games.push(game);
+      renderButtons();
+    }
   });
-$(document).on("click", ".game-btn", displayGameInfo);
-renderButtons();
+
+  $(document).on("click", ".game-btn", displayGameInfo);
 })
